438eeab7465ca2ccd93511c8eba66ce0e61d8729
Scott Miller <scott@millergeek.xyz>
2019-04-29 12:39:28 -0400

    init

---
+++ main.tf
@@ -0,0 +1,60 @@
+resource "aws_s3_bucket" "config_bucket" {
+  bucket = "${var.bucket}"
+  acl    = "private"
+
+  tags = "${merge(
+    local.common_tags,
+    var.extra_tags,
+    map(
+      "Name", "tf-state-bucket"
+    )
+  )}"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    id      = "expire"
+    enabled = true
+
+    noncurrent_version_expiration {
+      days = 90
+    }
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
+  bucket                  = "${aws_s3_bucket.config_bucket.id}"
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "terraform_statelock" {
+  name         = "${var.table}"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = "${merge(
+    local.common_tags,
+    var.extra_tags,
+    map(
+      "Name", "tf-state-lock-table"
+    )
+  )}"
+}
---
+++ variables.tf
@@ -0,0 +1,38 @@
+data "aws_caller_identity" "current" {}
+
+locals {
+  common_tags = {
+    Namespace = "${var.namespace}"
+    Owner     = "${var.owner}"
+  }
+}
+
+variable "region" {
+  type = "string"
+}
+
+variable "namespace" {
+  type    = "string"
+  default = ""
+}
+
+variable "owner" {
+  type    = "string"
+  default = ""
+}
+
+variable "extra_tags" {
+  description = "Mapping of any extra tags you want added to resources"
+  type        = "map"
+  default     = {}
+}
+
+variable "bucket" {
+  description = "Name of bucket to create"
+  type        = "string"
+}
+
+variable "table" {
+  description = "Name of Dynamo Table to create"
+  type        = "string"
+}
