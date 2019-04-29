8823922eae3731a8ce25045d9159ea9331da3a8c
Scott Miller <scott@millergeek.xyz>
2019-04-29 14:42:01 -0400

    change encryption and loggin

--- main.tf
+++ main.tf
@@ -26,9 +26,15 @@ resource "aws_s3_bucket" "config_bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
+        sse_algorithm     = "aws:kms"
+        kms_master_key_id = "aws/s3"
       }
     }
+  }
+
+  logging {
+    target_bucket = "${var.bucket}"
+    target_prefix = "TFStateLogs/"
   }
 }
 
