bb59072eb97c292ff66d426a15197438fad732be
Steven Black <genseb7@gmail.com>
2022-12-22 14:35:33 -0500

    add docs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
+
 output "external_id" {
   description = "External ID attached to IAM role in managed accounts"
   value       = local.external_id
@@ -11,4 +12,30 @@ output "kms_key_arn" {
 output "s3_bucket_backend" {
   description = "S3 bucket used to store TF state"
   value       = aws_s3_bucket.this.bucket
+}
+
+##########################################
+# stubs
+##########################################
+
+output "backend_config_stub" {
+  description = "Backend config stub to be used in child account(s)"
+  value       = <<EOF
+bucket         = "${aws_s3_bucket.this.bucket}"
+dynamodb_table = "${aws_dynamodb_table.this.name}"
+region         = "${local.region}"
+EOF
+}
+
+output "provider_config_stub" {
+  description = "Provider config stub to be used in child account(s)"
+  value       = <<EOF
+provider "aws" {
+  assume_role {
+    role_arn     = "arn:aws:iam::REPLACE_WITH_CHILD_ACCT_ID:role/${var.assumerole_role_name}"
+    session_name = "terraform-network"
+    external_id  = "${local.external_id}"
+  }
+}
+EOF
 }
